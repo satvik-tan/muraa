334745f1d8913a82a86c430f2316a5c382a3742f
bhavyajoshi <bjoshi1503@gmail.com>
2026-02-20 23:59:53 +0530

    fixes

--- frontend/src/hooks/use-toast.ts
+++ frontend/src/hooks/use-toast.ts
@@ -10,7 +10,7 @@ type ToastProps = {
 type ToastActionElement = React.ReactElement;
 
 const TOAST_LIMIT = 1;
-const TOAST_REMOVE_DELAY = 1000000;
+const TOAST_REMOVE_DELAY = 5000;
 
 type ToasterToast = ToastProps & {
   id: string;
